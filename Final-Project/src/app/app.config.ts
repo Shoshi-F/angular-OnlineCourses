f12f66efaad3dacc309d63b6cf4a7348b67c7e80
Shoshi-F <S0548469782@gmail.com>
2025-03-18 03:33:13 +0200

    first commit

---
+++ Final-Project/src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from '../intersceptor/token.interceptor';
+import { routes } from './app.routes';
+console.log(routes); // בדיקה אם מוגדר
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient() ,provideHttpClient(withInterceptors([tokenInterceptor]))
+
+  ]
+};
